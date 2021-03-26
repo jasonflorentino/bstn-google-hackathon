3be9b458052aca7f22458f29d179f5c6083cad6d
sam west <samwest0230@gmail.com>
2021-03-26 12:28:06 -0400

    hero page

--- src/pages/LandingPage/LandingPage.js
+++ src/pages/LandingPage/LandingPage.js
@@ -1,9 +1,10 @@
 import './LandingPage.scss';
+import Hero from '../../components/hero/hero'
 
 function LandingPage() {
   return (
     <div className="LandingPage">
-      LandingPage
+      <Hero />
     </div>
   );
 }

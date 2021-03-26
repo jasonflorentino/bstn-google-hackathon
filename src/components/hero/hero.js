4699cf10c74d6533648d94398e76ae8bbae21667
Jason Florentino <jasonmflorentino@gmail.com>
2021-03-26 15:31:32 -0400

    Update landing page style

--- src/components/hero/hero.js
+++ src/components/hero/hero.js
@@ -9,7 +9,7 @@ function Hero() {
     <section className='hero'>
       <div className="hero__info">
         <h1 className='hero__info--title'>Scheduel & Manage your Appointments in a Heartbeat</h1>
-        <h3 className='hero__info--para'>Google pulse allows for the convenience of booking and canceling appointments through the user’s calendar. The cancelation feature streamlines the process of freeing up available health care appointments to those who need them. Built in quick directions to navigate the user to the appointment on the quickest route.</h3>
+        <h3 className='hero__info--para'>Google pulse allows for the convenience of booking and canceling appointments through the your calendar. The cancellation feature streamlines the process of freeing up available health care appointments to those who need them. Built-in quick directions to navigate you to the appointment on the quickest route.</h3>
         <img className='hero__info--btn' src={btn} />
       </div>
           <img className='hero__pic' src={phone}/> 

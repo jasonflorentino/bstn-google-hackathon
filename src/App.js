455a5ef65102cc1ae7c93383c2dd245e8f4d27bd
sam west <samwest0230@gmail.com>
2021-03-26 13:53:15 -0400

    changed footer

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import LandingPage from "./pages/LandingPage/LandingPage";
 import MobileDemo from "./pages/MobileDemo/MobileDemo";
 import Nav from "./components/Nav/Nav";
-import Footer from './components/footer/footer'
+
 
 import './App.scss';
 
@@ -15,7 +15,6 @@ function App() {
           <Route path="/landingPage" component={LandingPage} />
           <Route path="/mobileDemo" component={MobileDemo} />
         </Switch>
-        <Footer />
       </BrowserRouter>
     </div>
   );

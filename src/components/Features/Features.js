cd3f86250161ba60eaf3aa1d706cf3b4cd1cd442
sam west <samwest0230@gmail.com>
2021-03-26 15:15:50 -0400

    updated landing page

--- src/components/Features/Features.js
+++ src/components/Features/Features.js
@@ -1,26 +1,33 @@
 import "./Features.scss";
+import bigPulse from '../../assets/pulse-logo.svg'
+import map from '../../assets/maps.svg'
+import booking from '../../assets/booking.svg'
+import cancel from '../../assets/cancel (2).svg'
 
 function Features() {
   return (
     <section className="Features">
+      <div className='Features__box'>
       <h2 className="Features__heading">FEATURES</h2>
+      <img src={bigPulse} />
+      </div>
       <div className="Features__container">
         <div className="feature">
-          <div className="feature__image" />
+          <img className='feature__img' src={booking} />
           <div className="feature__text">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          The ability to book a doctor's appointment without making a call
           </div>
         </div>
         <div className="feature">
-          <div className="feature__image" />
+          <img className='feature__img' src={cancel} />
           <div className="feature__text">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          Conveniently cancel an appointment allowing others in need of health care to access your appointment slot 
           </div>
         </div>
         <div className="feature">
-          <div className="feature__image" />
+          <img className='feature__img' src={map}/>
           <div className="feature__text">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          Make it easy for you to arrive to your appointments
           </div>
         </div>
       </div>
